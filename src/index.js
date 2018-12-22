2b729529215061c33bf940c069a43aba611cdeed
Joona Viertola <contact@joonaviertola.com>
2018-12-22 19:09:26 +0200

    Configuration for compiling React components

---
+++ src/index.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { Button } from './components';
+
+const root = document.getElementById('root');
+ReactDOM.render(
+  <Button>Click me</Button>,
+  root
+);
